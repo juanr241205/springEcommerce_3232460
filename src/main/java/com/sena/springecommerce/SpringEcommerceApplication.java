2d42e4a6e2497fe29c254667b1f2b723744b28e8
juanr241205 <juanr241205@gmail.com>
2025-11-10 17:34:59 -0500

    v0.5_claseAPIproductosPaqueteController_vargas_10112025

--- src/main/java/com/sena/springecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/sena/springecommerce/SpringEcommerceApplication.java
@@ -1,7 +1,9 @@
 package com.sena.springecommerce;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
